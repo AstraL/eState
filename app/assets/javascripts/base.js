00ffa3fa7586f6bd08b1e6a49b57b6e526bf3616
AstraL <sawyer126@gmail.com>
2017-12-28 05:27:51 +0200

    show share count

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,9 +1,7 @@
 $(document).on('turbolinks:load',function() {
     $('#share').jsSocials({
         url: window.location.href,
-        showCount: function(screenWidth) {
-            return (screenWidth > 1024);
-        },
+        showCount: true,
         showLabel: false,
         shares: ["facebook", "googleplus", "linkedin", "pocket", "whatsapp", "viber", "messenger", "vkontakte", "telegram"]
     });
